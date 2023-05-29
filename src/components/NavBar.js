982ee0e2027a525870c05378ccf99d78731c3f21
Seano <sean01234@yahoo.com>
2023-05-28 23:20:36 -0400

    display technologies

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+  const links=["home","about","projects"];
+  const mapLinks=links.map((linkObj) => {
+    return <a href={"#"+linkObj} key={linkObj}>{linkObj}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{mapLinks}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
-  console.log(projects);
+function ProjectList({projects}) {
+
+  const projItem=projects.map((projObj) => {
+    return <ProjectItem id={projObj.id} key={projObj.id} name={projObj.name} about={projObj.about} technologies={projObj.technologies} />
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">  {projItem}</div>
     </div>
   );
 }
